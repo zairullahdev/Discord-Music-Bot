8b7563b6d888005c6ccdd0e6271f227ff714abc3
KyuQ <114202052+KyuQ1602@users.noreply.github.com>
2022-10-17 20:23:48 +0700

    Change login
    Fix issue when bot try to go online but its throw some error

--- src/main.py
+++ src/main.py
@@ -22,12 +22,11 @@ async def on_ready():
 async def load_extensions():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            # cut off the .py from the file name
             await client.load_extension(f"cogs.{filename[:-3]}")
 
-async def TokenLogin():
+async def Login():
     async with client:
         await load_extensions()
         await client.start(botToken)
 
-asyncio.run(TokenLogin())
+asyncio.run(Login())
