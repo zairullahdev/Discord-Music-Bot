7256f85c1924786e68b6018ff361929cd71006bc
ZairullahDeveloper <99532718+zairullahdev@users.noreply.github.com>
2022-11-02 17:44:43 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ class OurBot(commands.Bot):
     async def setup_hook(self):
      await load_extensions()
 
-client = OurBot(command_prefix=os.environ['PREFIX'], intents = intents, status=discord.Status.idle, activity=discord.Streaming(name=f"Version {os.environ['BOTVER']} | {os.environ['PREFIX']}help", url="https://www.twitch.tv/discord"))
+client = OurBot(command_prefix=os.environ['BOTPREFIX'], intents = intents, status=discord.Status.idle, activity=discord.Streaming(name=f"Version {os.environ['BOTVER']} | {os.environ['BOTPREFIX']}help", url="https://www.twitch.tv/discord"))
 
 @client.event
 async def on_ready():
